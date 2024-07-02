0958911b93f6858afc0b0a3e6dde6b1b3cf3abea
sheldon <sheldson027@gmail.com>
2024-07-02 22:14:25 +0800

    Updated App.js to use HashRouter for GitHub Pages deployment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import SKUTrendVisualization from './SKUTrendVisualization';
 import CandlestickChart from './CandlestickChart';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/sku-trend-visualization">
       <div>
         <nav>
           <ul>
